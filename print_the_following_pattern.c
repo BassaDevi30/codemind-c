85a5b9127b78edad00348cf05d5f0e351e12f1b6
Bassa Devi Veera Naga Sri <97873322+BassaDevi30@users.noreply.github.com>
2022-02-22 23:29:17 +0530

    #include<stdio.h> int main() { int i,j,n;   scanf("%d",&n);   for(i=1;i<=n;i++)   {       for(j=1;j<=i;j++)       {           printf("%d",j);       }       printf(" ");   }   return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main()
-{ int n,i,j;
+{ int i,j,n;
   scanf("%d",&n);
   for(i=1;i<=n;i++)
   { 
-      for(j=1;j<=n-i+1;j++)
-      { printf("%d",j);
+      for(j=1;j<=i;j++)
+      { 
+          printf("%d",j);
       }
-    printf("
+      printf("
 ");
   }
-  return 2;
+  return 0;
 }
